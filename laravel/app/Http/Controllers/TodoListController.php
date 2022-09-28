f432cfb2b6fafbb49b881b1e27685f4859de2ce6
stitou-nada <nadastito1087@gmail.com>
2022-09-28 15:47:12 +0100

    delet

--- laravel/app/Http/Controllers/TodoListController.php
+++ laravel/app/Http/Controllers/TodoListController.php
@@ -7,11 +7,7 @@ use Illuminate\Http\Request;
 
 class TodoListController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function index()
     {
         $Data = TodoList::all();
@@ -20,59 +16,32 @@ class TodoListController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
         return view('insert');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     public function store(Request $request)
     {
         $nom = $request->input('Nom_Tach');
         $Desciption= $request->input('Description');
        
 
-        $contacts = TodoList::create([
+        $data = TodoList::create([
             "Nom_Tach" => $nom,
             "Description" => $Desciption
            
            
             ]);
-            if($contacts){
+            if($data){
                 return redirect('index');
                 
                }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $Data = TodoList::select("*")->where('id',$id)->get();
-        
-        return view("show",compact('Data'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit($id)
     {
         $Data = TodoList::select('*')->where("id",$id)->get();
@@ -80,19 +49,14 @@ class TodoListController extends Controller
         return view("edit",compact('Data'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(Request $request, $id)
     {
         $nom = $request->input('Nom_Tach');
         $Desciption= $request->input('Description');
 
         $Data = TodoList::where("id",$id)
+        // update : modifier
     ->update([
              "Nom_Tach" => $nom,
             "Description" => $Desciption
@@ -104,12 +68,7 @@ class TodoListController extends Controller
        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
         $delete = TodoList::where('id',$id)->delete();
