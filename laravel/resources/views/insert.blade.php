7b1306c03f2aa73eead1b8f9d0e6217d4b1fe811
stitou-nada <nadastito1087@gmail.com>
2022-09-28 15:21:41 +0100

    add

--- laravel/resources/views/insert.blade.php
+++ laravel/resources/views/insert.blade.php
@@ -4,7 +4,7 @@
 
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-<h4 class="card-title mb-4">Ajouter contact</h4>
+<h4 class="card-title mb-4">Ajouter une tache</h4>
 
 <div class="row">
     <div class="col-lg-12">
