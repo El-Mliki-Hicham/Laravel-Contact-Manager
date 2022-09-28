b87b20ced1faafc6dc537343bd5afae4c2a16d36
EL.MLIKI.HICHAM <elmliki.hicham@gmail.com>
2022-09-28 05:44:51 -0700

    reaclisation

---
+++ laravel/resources/views/Table.blade.php
@@ -0,0 +1,66 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+
+<section class="vh-100" style="background-color: #eee;">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-md-12 col-xl-10">
+  
+          <div class="card">
+            <div class="card-header p-3">
+              <h5 class="mb-0"><i class="fas fa-tasks me-2"></i>Task List</h5>
+            </div>
+            <div class="card-body" data-mdb-perfect-scrollbar="true" style="position: relative; height: 400px">
+  
+              <table class="table mb-0">
+                <thead>
+                  <tr>
+                    <th scope="col">Team Member</th>
+                    <th scope="col">Task</th>
+                    <th scope="col">Priority</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="fw-normal">
+                    <td>
+                     
+                      <span class="ms-2">Alice Mayer</span>
+                    </td>
+                    <td class="align-middle">
+                      <span>Call Sam For payments</span>
+                    </td>
+                    <td class="align-middle">
+                      <h6 class="mb-0"><span class="badge bg-danger">High priority</span></h6>
+                    </td>
+                    <td class="align-middle">
+                      <a href="#!" data-mdb-toggle="tooltip" title="Done"><i
+                          class="fas fa-check text-success me-3"></i></a>
+                      <a href="#!" data-mdb-toggle="tooltip" title="Remove"><i
+                          class="fas fa-trash-alt text-danger"></i></a>
+                    </td>
+                  </tr>
+                  
+               
+             
+                 
+               
+                    
+                
+                </tbody>
+              </table>
+  
+            </div>
+            <div class="card-footer text-end p-3">
+              <button class="me-2 btn btn-link">Cancel</button>
+              <button class="btn btn-primary">Add Task</button>
+            </div>
+          </div>
+  
+        </div>
+      </div>
+    </div>
+  </section>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/Table', function () {
+    return view('Table');
+});
 
 
 route::resource("index",TodoListController::class);
